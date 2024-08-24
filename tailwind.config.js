1629cb5248ad49950b690eca8faef0d7672e5623
gj701763 <gj701763@gmail.com>
2024-08-24 10:54:58 +0530

    Added assets as a submodule

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
